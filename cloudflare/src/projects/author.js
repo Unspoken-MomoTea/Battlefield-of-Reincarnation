79262a1625478aac9edecca220562c213f24ee8e
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-20 11:12:43 +0800

    feat: export unpublished project deletion

--- cloudflare/src/projects/author.js
+++ cloudflare/src/projects/author.js
@@ -1,3 +1,4 @@
 export { listOwnProjects } from './author/list.js';
 export { createProject, updateProject } from './author/metadata.js';
+export { deleteProject } from './author/delete.js';
 export { submitProjectForReview, uploadProjectVersion } from './author/versions.js';

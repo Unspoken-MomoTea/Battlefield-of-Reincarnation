1f112d7e45429ca06b820342d96ce24e84179bf2
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-20 12:52:41 +0800

    feat(workshop): export script installer helpers

--- src/CreativeWorkshop/services/installer.js
+++ src/CreativeWorkshop/services/installer.js
@@ -2,6 +2,7 @@ export { SHARED_WORLDBOOK_NAME } from './installer/constants.js';
 export { normalizeRegexArtifact } from './installer/normalize/regex.js';
 export { safePresetName } from './installer/normalize/preset.js';
 export { normalizeWorldbookArtifact } from './installer/normalize/worldbook.js';
+export { normalizeScriptArtifact, SCRIPT_SCOPES, scriptPrefix } from './installer/normalize/script.js';
 export { createWorkshopInstaller, workshopInstaller } from './installer/runtime.js';
 export { analyzeInstallConflicts } from './installer/conflicts.js';
 export { inspectInstalledProject, repairInstalledProject } from './installer/repair.js';

0964078837c3257bdd58b535258b37cbb5155655
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-20 13:01:05 +0800

    refactor(workshop): export project type helpers

--- cloudflare/src/projects/core.js
+++ cloudflare/src/projects/core.js
@@ -1,9 +1,9 @@
-export { ARTIFACT_KINDS, PROJECT_CATEGORIES } from './constants.js';
+export { ARTIFACT_KINDS, PROJECT_TYPES } from './constants.js';
 export { assertAdmin, getOwnedProject, writeAdminAudit } from './access.js';
 export { validateBundle } from './artifacts.js';
 export { parseDependencies, validateDependencies } from './dependencies.js';
 export {
-  adminPageParams, categoryField, nowSeconds, optionalText, pageParams,
+  adminPageParams, projectTypeField, nowSeconds, optionalText, pageParams,
   parseTags, slugField, tagsField, textField,
 } from './fields.js';
 export { buildManifest } from './manifest.js';

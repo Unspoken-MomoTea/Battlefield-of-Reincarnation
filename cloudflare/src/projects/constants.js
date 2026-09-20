d512df53dfcf277b193fee78b534d41fcaf9765d
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-20 13:01:01 +0800

    refactor(workshop): define character and extension project types

--- cloudflare/src/projects/constants.js
+++ cloudflare/src/projects/constants.js
@@ -1,4 +1,4 @@
-export const PROJECT_CATEGORIES = ['worldbook', 'regex', 'preset', 'data', 'mixed'];
+export const PROJECT_TYPES = ['character', 'extension'];
 export const ARTIFACT_KINDS = ['worldbook', 'regex', 'preset', 'script', 'data'];
 export const MAX_BUNDLE_BYTES = 4_000_000;
 export const MAX_ARTIFACTS = 32;

d4a26ee795cc119b0849341f63829fe323cac55f
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-20 16:52:35 +0800

    chore(workshop): bump publishing interaction fix to v1.5.1

--- src/CreativeWorkshop/app/workshop-app.js
+++ src/CreativeWorkshop/app/workshop-app.js
@@ -9,7 +9,7 @@ import { bindWorkshopEvents } from './events.js';
 import { createWorkshopViews } from './views.js';
 
 export const GLOBAL_NAME = 'ReincarnationWorkshop';
-export const WORKSHOP_VERSION = '1.5.0';
+export const WORKSHOP_VERSION = '1.5.1';
 
 let booted = false;
 

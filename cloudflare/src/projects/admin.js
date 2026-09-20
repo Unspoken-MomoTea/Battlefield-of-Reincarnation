a7118473fbdb144e27c983e3ac9606d79e0037fc
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-20 13:23:21 +0800

    refactor(workshop): remove legacy pending admin export

--- cloudflare/src/projects/admin.js
+++ cloudflare/src/projects/admin.js
@@ -1,5 +1,5 @@
 export { listAdminAuditLogs } from './admin/audit.js';
 export { getPendingProjectReview } from './admin/detail.js';
-export { listAdminProjects, listPendingProjects } from './admin/list.js';
+export { listAdminProjects } from './admin/list.js';
 export { setAdminProjectState } from './admin/moderation.js';
 export { reviewProject } from './admin/review.js';

ab7a4f434bd438467295ab0d3a1920edda5e65c9
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-22 00:23:39 +0800

    chore(workshop): bump author workflow redesign to v1.10.0

--- src/CreativeWorkshop/app/workshop-app.js
+++ src/CreativeWorkshop/app/workshop-app.js
@@ -10,7 +10,7 @@ import { bindWorkshopEvents } from './events.js';
 import { createWorkshopViews } from './views.js';
 
 export const GLOBAL_NAME = 'ReincarnationWorkshop';
-export const WORKSHOP_VERSION = '1.9.0';
+export const WORKSHOP_VERSION = '1.10.0';
 
 const CURRENT_SHA = (() => {
   const match = String(import.meta.url).match(

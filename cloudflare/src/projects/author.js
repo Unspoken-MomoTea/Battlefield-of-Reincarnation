72971d22d7abd2ad80d74410c2e1bb80d5550735
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-22 00:14:41 +0800

    feat(workshop): export author editor and visibility actions

--- cloudflare/src/projects/author.js
+++ cloudflare/src/projects/author.js
@@ -1,3 +1,5 @@
+export { setOwnerProjectVisibility } from './author/visibility.js';
+export { getOwnedProjectEditor } from './author/editor.js';
 export { listOwnProjects } from './author/list.js';
 export { createProject, updateProject } from './author/metadata.js';
 export { deleteProject } from './author/delete.js';

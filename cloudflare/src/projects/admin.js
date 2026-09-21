2385aeded5a01a3c047b4cbad32e7f3a4f1797e0
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-21 22:26:20 +0800

    feat(workshop): export admin project deletion

--- cloudflare/src/projects/admin.js
+++ cloudflare/src/projects/admin.js
@@ -1,3 +1,4 @@
+export { deleteAdminProject } from './admin/delete.js';
 export { listAdminAuditLogs } from './admin/audit.js';
 export { getPendingProjectReview } from './admin/detail.js';
 export { listAdminProjects } from './admin/list.js';

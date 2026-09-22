3dba8c588a54125883eb5455aaf4c7a36fab243d
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-22 10:58:26 +0800

    chore(workshop): bump channel-aware service to 0.13.0

--- cloudflare/src/index.js
+++ cloudflare/src/index.js
@@ -2,7 +2,7 @@ import { HttpError, json, withCors } from './http.js';
 import { guardRequest } from './middleware/request-guard.js';
 import { routeRequest } from './router.js';
 
-export const SERVICE_VERSION = '0.12.0';
+export const SERVICE_VERSION = '0.13.0';
 
 export async function handleRequest(request, env) {
   if (request.method === 'OPTIONS') {

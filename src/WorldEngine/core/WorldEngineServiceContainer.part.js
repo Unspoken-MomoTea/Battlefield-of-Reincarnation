d6490c8d87ed8b0fe3c894f121440fffa522b470
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-26 19:38:32 +0800

    refactor: compose causal and editor classes

--- src/WorldEngine/core/WorldEngineServiceContainer.part.js
+++ src/WorldEngine/core/WorldEngineServiceContainer.part.js
@@ -5,11 +5,13 @@
             this.events=new WorldEventService(engine);
             this.people=new WorldPersonActivityService(engine);
             this.history=new WorldHistoryService(engine);
+            this.causal=new WorldCausalService(engine);
             this.exploration=new WorldExplorationService(engine);
             this.rumor=new WorldRumorService(engine);
             this.requests=new WorldRequestService(engine);
             this.views=new WorldEngineViewRegistry(engine);
             this.prompts=new WorldPromptRegistry(engine);
+            this.editorController=new WorldEditorController(engine);
         }
         initialize(){
             this.prompts.initialize();

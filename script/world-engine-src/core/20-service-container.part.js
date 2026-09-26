04bc87e9b11863aa894c20f1f0275c291b8b4c67
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-26 15:19:46 +0800

    refactor: compose feature registry into world services

--- script/world-engine-src/core/20-service-container.part.js
+++ script/world-engine-src/core/20-service-container.part.js
@@ -7,21 +7,28 @@
             this.personEditor=new WorldPersonEditor(engine,this.mutations);
             this.prompts=new WorldPromptRegistry(engine);
             this.promptRuntime=new WorldPromptRuntimeAdapter(engine,this.prompts);
+            this.features=new WorldEngineFeatureRegistry(engine);
         }
         initialize(){
             this.prompts.initializeConfig();
             this.promptRuntime.initialize();
+            this.features.initialize();
             return this;
         }
         bindPanel(){
             this.eventEditor.bindPanel();
             this.personEditor.bindPanel();
+            this.features.bindPanel();
         }
         afterRender(){
             this.eventEditor.afterRender();
             this.personEditor.afterRender();
+            this.features.afterRender();
         }
+        async modifyRequest(request,base){return this.features.modifyRequest(request,base);}
+        async afterCatalogue(result){return this.features.afterCatalogue(result);}
         dispose(){
+            this.features.dispose();
             this.eventEditor.dispose?.();
             this.personEditor.dispose?.();
         }

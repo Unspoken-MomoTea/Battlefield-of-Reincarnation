3d7899fd656cea942d083012fc5d0c221aebfbc7
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-26 21:14:08 +0800

    refactor: register phase3 composed features

--- src/WorldEngine/core/WorldEngineServiceContainer.part.js
+++ src/WorldEngine/core/WorldEngineServiceContainer.part.js
@@ -12,9 +12,17 @@
             this.views=new WorldEngineViewRegistry(engine);
             this.prompts=new WorldPromptRegistry(engine);
             this.editorController=new WorldEditorController(engine);
+            this.features=new WorldEngineFeatureRegistry(engine);
+            this.apiPreset=new WorldApiPresetController(engine);
+            this.causalOverview=new WorldCausalOverviewController(engine);
+            this.npcAuditPrompt=new WorldNpcAuditPromptFeature(engine);
+            this.features.register('npcAuditPrompt',this.npcAuditPrompt);
+            this.features.register('apiPreset',this.apiPreset);
+            this.features.register('causalOverview',this.causalOverview);
         }
         initialize(){
             this.prompts.initialize();
+            this.features.initialize();
             return this;
         }
     }

c4bd3bdfbda6d849a6968e20bb5d8a7c106e4152
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-26 20:29:58 +0800

    refactor: compose feature registry into world services

--- src/WorldEngine/core/WorldEngineServiceContainer.part.js
+++ src/WorldEngine/core/WorldEngineServiceContainer.part.js
@@ -12,9 +12,18 @@
             this.views=new WorldEngineViewRegistry(engine);
             this.prompts=new WorldPromptRegistry(engine);
             this.editorController=new WorldEditorController(engine);
+            this.features=new WorldEngineFeatureRegistry(engine);
         }
         initialize(){
             this.prompts.initialize();
+            this.features.initialize();
             return this;
         }
+        afterInit(){this.features.afterInit();}
+        bindPanel(){this.features.bindPanel();}
+        beforeRender(){this.features.beforeRender();}
+        afterRender(){this.features.afterRender();}
+        dispose(){this.features.dispose();}
+        modifyRequest(request,base){return this.features.modifyRequest(request,base);}
+        afterCatalogue(result){return this.features.afterCatalogue(result);}
     }

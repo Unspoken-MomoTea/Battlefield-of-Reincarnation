d189e473c8000980d95c134130e39d2dff53804d
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-27 00:33:24 +0800

    build: register world run orchestrator

--- tools/build-world-engine.py
+++ tools/build-world-engine.py
@@ -68,6 +68,7 @@ PARTS = (
     '@src/WorldEngine/domains/WorldRequestService.part.js',
     '@src/WorldEngine/domains/WorldApiTransportService.part.js',
     '@src/WorldEngine/domains/WorldPromptDocumentService.part.js',
+    '@src/WorldEngine/domains/WorldRunOrchestrator.part.js',
     '@src/WorldEngine/domains/WorldRequestFeature.part.js',
     '@src/WorldEngine/domains/WorldAutoProgressController.part.js',
     '@src/WorldEngine/domains/WorldReplayService.part.js',

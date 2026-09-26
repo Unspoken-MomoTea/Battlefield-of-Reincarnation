6e3894372e275952aadfee7046d74d937b7f293b
Unspoken-MomoTea <bayuexue.us@gmail.com>
2026-09-26 20:15:21 +0800

    refactor: remove prompt prototype adapter from service container

--- script/world-engine-src/core/20-service-container.part.js
+++ script/world-engine-src/core/20-service-container.part.js
@@ -6,12 +6,10 @@
             this.eventEditor=new WorldEventEditor(engine,this.mutations);
             this.personEditor=new WorldPersonEditor(engine,this.mutations);
             this.prompts=new WorldPromptRegistry(engine);
-            this.promptRuntime=new WorldPromptRuntimeAdapter(engine,this.prompts);
             this.features=new WorldEngineFeatureRegistry(engine);
         }
         initialize(){
             this.prompts.initializeConfig();
-            this.promptRuntime.initialize();
             this.features.initialize();
             return this;
         }
